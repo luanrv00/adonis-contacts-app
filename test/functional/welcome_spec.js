ba0c4e1f70376763009c9c8d6b41bb989a20acaf
Luan F. R. Vicente <luanrvmood@gmail.com>
2021-01-27 13:31:04 -0300

    Links welcome page to the contact form

--- test/functional/welcome_spec.js
+++ test/functional/welcome_spec.js
@@ -3,4 +3,11 @@ describe('Welcome page', () => {
     cy.visit('/')
     cy.contains('Ver lista de contatos')
   })
+
+  it('has a link to contact form', () => {
+    cy.visit('/')
+    cy.contains('Adicionar novo contato')
+    cy.get('.add').click()
+    cy.url().should('include', '/contacts/create')
+  })
 })
